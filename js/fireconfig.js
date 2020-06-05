789ea39cd3a6999ac6dcc83409148ed6ed1e772d
kromate <akpananthony33@gmail.com>
2020-06-05 15:39:37 +0100

    adjustment

---
+++ js/fireconfig.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBtqzh4J8CBqwgEGamQLlI6Gd0p5dVTmqo",
+  authDomain: "timely-60c93.firebaseapp.com",
+  databaseURL: "https://timely-60c93.firebaseio.com",
+  projectId: "timely-60c93",
+  storageBucket: "timely-60c93.appspot.com",
+  messagingSenderId: "760523832372",
+  appId: "1:760523832372:web:f8caa259f618b96fed1e61",
+  measurementId: "G-WMD746W6C8"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
